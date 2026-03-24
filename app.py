0da7b1ed804313d582cc1ecad143f72791e0440f
Teo Demiri <teo.demiri12@gmail.com>
2026-03-24 23:54:05 +0100

    Transforming the data

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from langchain_community.document_loaders import AsyncHtmlLoader
-url = "https://packaging.python.org/en/latest/"
+from langchain_community.document_transformers import Html2TextTransformer
+
+"""
+Load HTML from url and transform it into plain text
+"""
+
+url = "url" #here goes the link
 loader = AsyncHtmlLoader(url)
 data = loader.load()
-print(data)
+
+#print(data)
+
+transformer = Html2TextTransformer()
+transformed_data = transformer.transform_documents(data)
+print(transformed_data[0].page_content)     
+print(transformed_data[0].metadata)         
