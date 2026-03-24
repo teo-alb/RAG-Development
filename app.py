5518662bc5f135ca781fbb097a7ce6c01594d2ab
Teo Demiri <teo.demiri12@gmail.com>
2026-03-25 00:11:51 +0100

    Chunking Section added to the Indexing Pipeline

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from langchain_community.document_loaders import AsyncHtmlLoader
 from langchain_community.document_transformers import Html2TextTransformer
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 """
 Load HTML from url and transform it into plain text
 """
 
-url = "url" #here goes the link
+url = "https://example.com" #here goes the url
 loader = AsyncHtmlLoader(url)
 data = loader.load()
 
@@ -15,3 +16,19 @@ transformer = Html2TextTransformer()
 transformed_data = transformer.transform_documents(data)
 print(transformed_data[0].page_content)     
 print(transformed_data[0].metadata)         
+
+"""
+Chunking - Splitting the text
+"""
+
+text_splitter = RecursiveCharacterTextSplitter(
+    separators = ["\n\n", "\n", ".", " "],
+    chunk_size = 1000,
+    chunk_overlap = 200    #20% overlap
+)
+
+chunks = text_splitter.create_documents(
+    [transformed_data[0].page_content]
+)
+
+print(len(chunks)) #number of chunks created
